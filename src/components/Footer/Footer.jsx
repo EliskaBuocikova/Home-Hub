6b7391f902a206f54cc4b6194608fa6a852577c3
Tereza Zahradníková <tereza.zahradnikova@centrum.cz>
2023-06-05 19:49:33 +0200

    header and footer modified, icons to header added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import './style.css';
 
 const Footer = () => {
     return(
-        <footer class="footer">Czechitas, Digitální akademie: Web - jaro 2023</footer>
+        <footer className="footer">
+            <img className="icon__home" src="../img/home-icon.png" alt="home_icon"></img>
+            <img className="icon__notifications" src="../img/notifications-icon.png" alt="notifications_icon"></img>
+            <p className="footer__text">Czechitas, Digitální akademie: Web - jaro 2023</p>
+        </footer>
     )
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,12 @@ const Header = () => {
         <header className="header">
             <h1 className="header__logo">HH</h1>
             <div className="header__nav">
-                <nav className="nav__household">Household</nav>
-                <nav className="nav__wallet">Wallet</nav>
-                <nav className="nav__car">Car</nav>
+                <nav className="nav__categories">Categories</nav>
+                <nav className="nav__notifications">Notifications</nav>
             </div>
             <p className="header__login">login</p>
         </header>
     )
 }
 
-export default Header;
+export default Header
