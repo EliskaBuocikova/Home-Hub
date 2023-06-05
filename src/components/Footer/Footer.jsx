7b020f623df8b871b3bf4f1ab598660ec489da30
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-05 20:45:37 +0200

    opravení footer - zobrazení icon na mobilu

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React from 'react';
 import './style.css';
+import homeIcon from '../../img/home-icon.png';
+import notificationsIcon from '../../img/notifications-icon.png';
 
 const Footer = () => {
-    return(
-        <footer className="footer">
-            <img className="icon__home" src="../img/home-icon.png" alt="home_icon"></img>
-            <img className="icon__notifications" src="../img/notifications-icon.png" alt="notifications_icon"></img>
-            <p className="footer__text">Czechitas, Digitální akademie: Web - jaro 2023</p>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <img className="icon__home" src={homeIcon} alt="home_icon"></img>
+      <img
+        className="icon__notifications"
+        src={notificationsIcon}
+        alt="notifications_icon"
+      ></img>
+      <p className="footer__text">
+        Czechitas, Digitální akademie: Web - jaro 2023
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
