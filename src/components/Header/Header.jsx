d92202613588127457df10d490456911d1a40ac9
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-06 12:47:46 +0200

    přidání obou bannerů k headeru, nastavení media na bannery, opravení ikony ve footeru

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import bannerMobil from '../../img/banner-mobil.png';
+import backBanner from '../../img/banner-desktop.png';
 import './style.css';
 
 const Header = () => {
   return (
-    <header className="header">
-      <h1 className="header__logo">HH</h1>
-      <div className="header__nav">
-        <nav className="nav__categories">Categories</nav>
-        <nav className="nav__notifications">Notifications</nav>
-      </div>
-      <p className="header__login">login</p>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="header__logo">HH</h1>
+        <div className="header__nav">
+          <nav className="nav__categories">Categories</nav>
+          <nav className="nav__notifications">Notifications</nav>
+        </div>
+        <p className="header__login">login</p>
+      </header>
+      <img className="ban__mobil" src={bannerMobil} alt="" />
+      <img className="back__image" src={backBanner} alt="" />
+    </>
   );
 };
 
