098bb3898eb750343449379a67d4d77f673a7fe5
Tereza Zahradníková <tereza.zahradnikova@centrum.cz>
2023-06-06 13:05:11 +0200

    vytvořena komponenta Banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './style.css';
+
+const Banner = () => {
+    return(
+        <div className="banner">
+            <div className="banner__img"></div>
+            <div className="banner__bar">
+                <p className="bar__text">Categories</p>
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
