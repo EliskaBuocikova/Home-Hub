369880d96b791be7f9d59d1168d97a4957eaa079
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-06 17:35:28 +0200

    uprava dvou ikon, přidání lišty k banneru na mobilu

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React from 'react';
 import './style.css';
 
 const Banner = () => {
-    return(
-        <div className="banner">
-            <div className="banner__img"></div>
-        </div>
-    )
-}
+  return (
+    <div className="banner">
+      <div className="banner__img"></div>
+      <div className="siteName"></div>
+    </div>
+  );
+};
 
 export default Banner;
