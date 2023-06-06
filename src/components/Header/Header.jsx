073a84f91fff1e44daf9c4d02f4805f499183d23
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-07 00:25:01 +0200

    oživení menu v headeru

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import './style.css';
 import logoWhite from '../../img/logo-H-bile.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="header">
       <img className="header__logo" src={logoWhite} alt="home_icon"></img>
       <div className="header__nav">
-        <nav className="nav__categories">Categories</nav>
-        <nav className="nav__notifications">Notifications</nav>
+        <nav className="header__menu">
+          <Link className="nav__categories" to="/categories">
+            Categories
+          </Link>
+          <span> | </span>
+          <Link className="nav__notifications" to="/">
+            Notifications
+          </Link>
+        </nav>
       </div>
       <p className="header__login">login</p>
     </header>
