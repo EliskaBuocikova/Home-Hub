c3be5a783fc1f6ef7fae8ff032972f289ce547ba
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-06 13:07:18 +0200

    vymazání commitu od DS, odstranění stylování banneru

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import bannerMobil from '../../img/banner-mobil.png';
-import backBanner from '../../img/banner-desktop.png';
 import './style.css';
 import logoWhite from '../../img/logo-H-bile.png';
 
