136eea498e30daa3f37aef166917e76cece2c69f
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-06 23:46:48 +0200

    routování n.2

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom';
 import './style.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
-import Categories from './components/Categories/Categories';
+
 import Login from './components/Login/Login.jsx';
-import ItemList from './components/ItemList/ItemList.jsx';
 import HomePage from './components/HomePage/HomePage';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Categories from './components/Categories/Categories';
+import ItemList from './components/ItemList/ItemList.jsx';
 
 const App = () => {
   return (
@@ -16,11 +18,12 @@ const App = () => {
         <Header />
 
         <main className="main">
-          <Categories />
           <Routes>
             <Route exact path="/" element={<HomePage />} />
+            <Route path="/categories" element={<Categories />} />
             <Route path="/items" element={<ItemList />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/homepage" element={<HomePage />} />
           </Routes>
         </main>
 
