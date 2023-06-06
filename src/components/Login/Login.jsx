79be6d38f7488401737528263f18e138649d221e
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-06 23:36:10 +0200

    stylování Login stránky

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import './style.css';
+import Banner from '../Banner/Banner';
+import logo from '../../img/logo-cele-cerne.png';
 
 const Login = () => {
   return (
-    <div className="login">
-      <div className="login__logo"></div>
-      <p className="login__text">
-        Aplikace umožňuje uživatelům mít provoz své domácnosti snadno pod
-        kontrolou.
-      </p>
-      <form className="login__form">
-        <input type="text" placeholder="user name" />
-        <input type="text" placeholder="password" />
-        <button className="login__button">log in</button>
-      </form>
-      <footer>
-        <p>Don't have an account?</p>
-        <p>
-          Register <a href="#"> HERE</a>
+    <>
+      <Banner />
+      <div className="login">
+        <div className="login__logo">
+          <img className="login__logo" src={logo} />
+        </div>
+        <p className="login__text">
+          Aplikace umožňuje uživatelům mít provoz své domácnosti snadno pod
+          kontrolou.
         </p>
-      </footer>
-    </div>
+        <form className="login__form">
+          <input type="text" placeholder="user name" />
+          <input type="text" placeholder="password" />
+          <button className="login__button">log in</button>
+        </form>
+        <footer>
+          <p className="login__question">Don't have an account?</p>
+          <p className="login__link">
+            Register <a href="#"> HERE</a>
+          </p>
+        </footer>
+      </div>
+    </>
   );
 };
 
--- src/index.jsx
+++ src/index.jsx
@@ -4,22 +4,20 @@ import './style.css';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 import Categories from './components/Categories/Categories';
-
+import Login from './components/Login/Login';
 
 const App = () => {
-  return(
-  
-  <div className="container">
-      <Header/>
+  return (
+    <div className="container">
+      <Header />
 
-    <main className="main">
-      <Categories/>
-    </main>
+      <main className="main">
+        <Login />
+      </main>
 
-    <Footer/>
-
-  </div>
-  )
+      <Footer />
+    </div>
+  );
 };
 
 createRoot(document.querySelector('#app')).render(<App />);
