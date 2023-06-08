0f1a1072fb426ab92156bec0a889f33f2d1cf897
Tereza Zahradníková <tereza.zahradnikova@centrum.cz>
2023-06-08 20:30:16 +0200

    přidán obsah komponenty HomePage, upraveno stylování

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -8,6 +8,42 @@ const HomePage = () => {
     <>
       <Banner />
       <PageName title={'Homepage'} />
+      
+      <div class="homepage__container">
+                <h2 class="homepage__datetime">Today is Monday, 12 June 2023</h2>
+                <p class="homepage__prompt">What's coming up in the next two months?</p>
+            </div>
+
+      <div className="notifications__wrapper">
+                <div className="notifications">
+                        <div class="notification">
+                            <p class="notification__date">12.3.</p>
+                            <p class="notification__text">Záruka pračka</p>
+                            <div class="notification__tick"></div>
+                        </div>
+                        <div class="notification">
+                            <p class="notification__date">12.3.</p>
+                            <p class="notification__text">Záruka pračka</p>
+                            <div class="notification__tick"></div>
+                        </div>
+                        <div class="notification">
+                            <p class="notification__date">12.3.</p>
+                            <p class="notification__text">Záruka pračka</p>
+                            <div class="notification__tick"></div>
+                        </div>
+                        <div class="notification">
+                            <p class="notification__date">12.3.</p>
+                            <p class="notification__text">Záruka pračka</p>
+                            <div class="notification__tick"></div>
+                        </div>
+                        <div class="notification">
+                            <p class="notification__date">12.3.</p>
+                            <p class="notification__text">Záruka pračka</p>
+                            <div class="notification__tick"></div>
+                        </div>
+                        <div class="notifications__history">History</div>
+                    </div>
+                    </div>
     </>
   );
 };
