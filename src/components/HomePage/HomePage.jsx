76f33158a6039e770257a151aac3f67707c2df0b
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-09 14:16:07 +0200

    upravení shimmeru na formuláři, tlačítko history navázáno na history

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageName from '../PageName/PageName';
 import Banner from '../Banner/Banner.jsx';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const HomePage = () => {
@@ -8,42 +9,44 @@ const HomePage = () => {
     <>
       <Banner />
       <PageName title={'Homepage'} />
-      
+
       <div class="homepage__container">
-                <h2 class="homepage__datetime">Today is Monday, 12 June 2023</h2>
-                <p class="homepage__prompt">What's coming up in the next two months?</p>
-            </div>
+        <h2 class="homepage__datetime">Today is Monday, 12 June 2023</h2>
+        <p class="homepage__prompt">What's coming up in the next two months?</p>
+      </div>
 
       <div className="notifications__wrapper">
-                <div className="notifications">
-                        <div class="notification">
-                            <p class="notification__date">12.3.</p>
-                            <p class="notification__text">Záruka pračka</p>
-                            <div class="notification__tick"></div>
-                        </div>
-                        <div class="notification">
-                            <p class="notification__date">12.3.</p>
-                            <p class="notification__text">Záruka pračka</p>
-                            <div class="notification__tick"></div>
-                        </div>
-                        <div class="notification">
-                            <p class="notification__date">12.3.</p>
-                            <p class="notification__text">Záruka pračka</p>
-                            <div class="notification__tick"></div>
-                        </div>
-                        <div class="notification">
-                            <p class="notification__date">12.3.</p>
-                            <p class="notification__text">Záruka pračka</p>
-                            <div class="notification__tick"></div>
-                        </div>
-                        <div class="notification">
-                            <p class="notification__date">12.3.</p>
-                            <p class="notification__text">Záruka pračka</p>
-                            <div class="notification__tick"></div>
-                        </div>
-                        <div class="notifications__history">History</div>
-                    </div>
-                    </div>
+        <div className="notifications">
+          <div class="notification">
+            <p class="notification__date">12.3.</p>
+            <p class="notification__text">Záruka pračka</p>
+            <div class="notification__tick"></div>
+          </div>
+          <div class="notification">
+            <p class="notification__date">12.3.</p>
+            <p class="notification__text">Záruka pračka</p>
+            <div class="notification__tick"></div>
+          </div>
+          <div class="notification">
+            <p class="notification__date">12.3.</p>
+            <p class="notification__text">Záruka pračka</p>
+            <div class="notification__tick"></div>
+          </div>
+          <div class="notification">
+            <p class="notification__date">12.3.</p>
+            <p class="notification__text">Záruka pračka</p>
+            <div class="notification__tick"></div>
+          </div>
+          <div class="notification">
+            <p class="notification__date">12.3.</p>
+            <p class="notification__text">Záruka pračka</p>
+            <div class="notification__tick"></div>
+          </div>
+          <Link className="notifications__history" to="/history">
+            History
+          </Link>
+        </div>
+      </div>
     </>
   );
 };
