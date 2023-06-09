033897f130f07b082257cf3ddc2d5902c9e929d0
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-09 14:45:55 +0200

    oživení přesměrování ze stránky Categories na stránku ItemList

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -7,6 +7,7 @@ import electronicsIcon from '../../img/electronics-icon-T.png';
 import carIcon from '../../img/car-icon-T.png';
 import walletIcon from '../../img/wallet-icon-T.png';
 import othersIcon from '../../img/others-icon-T.png';
+import { Link } from 'react-router-dom';
 
 const Categories = () => {
   return (
@@ -14,46 +15,46 @@ const Categories = () => {
       <Banner />
       <PageName title={'Categories'} />
       <div className="categories">
-        <div className="category" id="appliances">
+        <Link className="category" id="appliances" to="/itemlist">
           <img
             className="icon__appliances"
             src={appliancesIcon}
             alt="appliances_icon"
           ></img>
           Appliances
-        </div>
+        </Link>
 
-        <div className="category" id="electronics">
+        <Link className="category" id="electronics" to="/itemlist">
           <img
             className="icon__electronics"
             src={electronicsIcon}
             alt="electronics_icon"
           ></img>
           Electronics
-        </div>
+        </Link>
 
-        <div className="category" id="car">
+        <Link className="category" id="car" to="/itemlist">
           <img className="icon__car" src={carIcon} alt="car_icon"></img>
           Car
-        </div>
+        </Link>
 
-        <div className="category" id="wallet">
+        <Link className="category" id="wallet" to="/itemlist">
           <img
             className="icon__wallet"
             src={walletIcon}
             alt="wallet_icon"
           ></img>
           Wallet
-        </div>
+        </Link>
 
-        <div className="category" id="others">
+        <Link className="category" id="others" to="/itemlist">
           <img
             className="icon__others"
             src={othersIcon}
             alt="others_icon"
           ></img>
           Others
-        </div>
+        </Link>
       </div>
     </>
   );
