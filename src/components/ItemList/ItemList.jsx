2c63c9bc499b4809c750df9bbf18a16ea88b81e1
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-10 15:08:27 +0200

    napojení itemlist na supabase

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -5,8 +5,17 @@ import folderAddIcon from '../../img/folder-add-icon.svg';
 import magnifyingGlassIcon from '../../img/magnifying-glass-icon.png';
 import { Link } from 'react-router-dom';
 import './style.css';
+import { fetchListItem } from '../../apiFunctions/formItem';
+import { useEffect, useState } from 'react';
 
 const ItemList = ({ name }) => {
+  const [itemlist, setItemList] = useState([]);
+
+  useEffect(() => {
+    fetchListItem().then(setItemList);
+  }, []);
+  console.log(itemlist);
+
   return (
     <>
       <PageName title={'...Jméno kategorie...'} />
@@ -14,42 +23,30 @@ const ItemList = ({ name }) => {
         <form>
           <input type="text" placeholder="search" />
           <button className="magnifying-glass-icon">
-            <img className="magnifying-glass-pic" src={magnifyingGlassIcon} />
+            <img
+              className="magnifying-glass-pic"
+              src={magnifyingGlassIcon}
+              alt="Magnifying Glass"
+            />
           </button>
         </form>
         <div className="item-list__folders">
-          <Link className="item-list__item" to="/itemdetail">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">lednice{name}</p>
-          </Link>
-
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">mixér{name}</p>
-          </div>
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">pračka{name}</p>
-          </div>
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">žehlička{name}</p>
-          </div>
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">ledovač{name}</p>
-          </div>
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">nevim{name}</p>
-          </div>
-          <div className="item-list__item">
-            <img className="item-list__folder-pic" src={folderIcon} />
-            <p className="item-list__item-name">nevim 2{name}</p>
-          </div>
-
+          {itemlist.map((item) => (
+            <Link className="item-list__item" to="/itemdetail" key={item.id}>
+              <img
+                className="item-list__folder-pic"
+                src={folderIcon}
+                alt="Folder Icon"
+              />
+              <p className="item-list__item-name">{item.name}</p>
+            </Link>
+          ))}
           <Link className="item-list__item" to="/form">
-            <img className="item-list__folder-add-pic" src={folderAddIcon} />
+            <img
+              className="item-list__folder-add-pic"
+              src={folderAddIcon}
+              alt="Folder Add Icon"
+            />
           </Link>
         </div>
       </div>
