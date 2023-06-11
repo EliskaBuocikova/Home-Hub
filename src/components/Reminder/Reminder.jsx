31d907b6500e0046236a75594b81763329cd7690
Daniela Schneiderová <kozlovadaniela8@gmail.com>
2023-06-11 19:18:19 +0200

    opravení datumu v reminders

--- src/components/Reminder/Reminder.jsx
+++ src/components/Reminder/Reminder.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useState } from 'react';
 import { updateItem } from '../../apiFunctions/formItem';
 import { checkReminder } from '../../apiFunctions/formItem';
+import dayjs from 'dayjs';
 
 const Reminder = ({ reminder, onReminderChecked }) => {
   const [isChecked, setIsChecked] = useState(reminder.checked);
@@ -12,10 +13,10 @@ const Reminder = ({ reminder, onReminderChecked }) => {
       onReminderChecked();
     });
   };
-
+  const date = dayjs(reminder.items.dueDate);
   return (
     <div className="notification">
-      <p className="notification__date">{reminder.items.dueDate}</p>
+      <p className="notification__date">{date.format('MMMM DD, YYYY')}</p>
       <p className="notification__text">{reminder.items.reminderNote}</p>
       <div
         className={`notification__tick-${isChecked ? 'show' : 'hide'}`}
