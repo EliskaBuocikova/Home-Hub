74e981b42d4e954ea1c5a5ffc4db8f778e9ca663
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-17 11:43:58 +0200

    přidání key

--- src/apiFunctions/supabase.js
+++ src/apiFunctions/supabase.js
@@ -3,5 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const SUPABASE_URL = 'https://uzvkkculocyjknnvdmyq.supabase.co';
 
 export const getSupabase = () => {
-  return createClient(SUPABASE_URL, localStorage.getItem('key'));
+  return createClient(
+    SUPABASE_URL,
+    localStorage.getItem(
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dmtrY3Vsb2N5amtubnZkbXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzMjM2NjUsImV4cCI6MjAwMTg5OTY2NX0.iV5H0mmUiPQjfjRvfdnO_jPq9PlgccNjYdWfAIuhXIU',
+    ),
+  );
 };
