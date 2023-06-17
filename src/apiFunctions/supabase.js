867c8889ae3f3b92e2c1691a3ce16fe00fab2333
Eliška Buociková <buocikovaeliska@gmail.com>
2023-06-17 11:49:36 +0200

    změna key

--- src/apiFunctions/supabase.js
+++ src/apiFunctions/supabase.js
@@ -2,11 +2,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://uzvkkculocyjknnvdmyq.supabase.co';
 
+const SUPABASE_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dmtrY3Vsb2N5amtubnZkbXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzMjM2NjUsImV4cCI6MjAwMTg5OTY2NX0.iV5H0mmUiPQjfjRvfdnO_jPq9PlgccNjYdWfAIuhXIU';
+
 export const getSupabase = () => {
-  return createClient(
-    SUPABASE_URL,
-    localStorage.getItem(
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV6dmtrY3Vsb2N5amtubnZkbXlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODYzMjM2NjUsImV4cCI6MjAwMTg5OTY2NX0.iV5H0mmUiPQjfjRvfdnO_jPq9PlgccNjYdWfAIuhXIU',
-    ),
-  );
+  return createClient(SUPABASE_URL, SUPABASE_KEY);
 };
